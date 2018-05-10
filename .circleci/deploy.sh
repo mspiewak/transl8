b19884b47b132e64892f4c54dd30bbf545da3129
Marcin Spiewak <mspiewak85@gmail.com>
2018-05-10 11:25:35 +0100

    fixed issue with aws deployment

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -41,7 +41,7 @@ OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_NAME --o
 OLD_TASK_DEF_REVISION=$(echo $OLD_TASK_DEF | jq ".taskDefinition|.revision")
 
 echo "dropping in the new image"
-NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $DOCKER_IMAGE '.taskDefinition.containerDefinitions[2].image=$NDI')
+NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
 
 echo "create a new task template with all the required information to bring over"
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
