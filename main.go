9cea4e289f6538710edb0c283a7c62d806080ead
Marcin Spiewak <mspiewak85@gmail.com>
2018-05-10 11:02:48 +0100

    added empty translate handler

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type app struct{}
+
 func main() {
+	var app app
 	r := mux.NewRouter()
+	r.Handle("/translate", app.translateHandler()).Methods(http.MethodPost)
 
 	log.Println("listening")
 	log.Fatal(http.ListenAndServe(":9010", commonHeaders(r)))
+}
+
+func (a *app) translateHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(bodyBytes)
+	}
 }
 
 func commonHeaders(next http.Handler) http.Handler {
